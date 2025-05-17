73bc65a59581142d5607c2f662bd363bbf500168
Federico Trani <federicotrani@gmail.com>
2025-05-16 22:48:51 -0300

    Agregar archivos de proyecto.

---
+++ DemoCollectionView/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoCollectionView
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DemoCollectionView/Models/Producto.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DemoCollectionView.Models;
+
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public float Precio { get; set; }
+    public int Stock { get; set; }
+    public string Imagen { get; set; }
+}
