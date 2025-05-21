8250bf18d6c6f2b78bffbbe81d452c91b304355b
Federico Trani <federicotrani@gmail.com>
2025-05-20 22:54:42 -0300

    Cambios lista productos

--- DemoCollectionView/Views/ListaProductosPage.xaml.cs
+++ DemoCollectionView/Views/ListaProductosPage.xaml.cs
@@ -22,9 +22,15 @@ public partial class ListaProductosPage : ContentPage
         {
             new Producto { Id = 1, Nombre = "Pan Salvado", Precio = 1500, Stock = 5, Imagen = "pan_salvado.jpg" },
             new Producto { Id = 2, Nombre = "Pan Lactal", Precio = 1800, Stock = 3, Imagen = "pan_lactal.jpg" },
-            new Producto { Id = 3, Nombre = "Tomate Perita", Precio = 2800, Stock = 8, Imagen = "Tomate Perita.jpg" },
+            new Producto { Id = 3, Nombre = "Tomate Perita", Precio = 2800, Stock = 8, Imagen = "tomate_perita.jpg" },
             new Producto { Id = 4, Nombre = "Yerba Taragui", Precio = 3145, Stock = 2, Imagen = "yerba_taragui.jpg" },
             new Producto { Id = 5, Nombre = "Yerba Rosamonte", Precio = 3266, Stock = 10, Imagen = "yerba_rosamonte.jpg" }
         };
+    }
+
+    public void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // DisplayAlert("Producto Seleccionado", "El producto seleccionado es: " + e.CurrentSelection[0].ToString(), "OK");
+        DisplayAlert("Producto Seleccionado", "El producto seleccionado es: " + productoSeleccionado.Nombre, "OK");
     }
 }
